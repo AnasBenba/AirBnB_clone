20170a4a5a0967b7682f5ad091c40405a651a89b
AnasBenba <anasbenba123@gmail.com>
2023-07-14 03:47:41 -0700

    Added the user class

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """subclass of BaseModel"""
+
+    def __init__(self):
+        self.email = ''
+        self.password = ''
+        self.first_name = ''
+        self.last_name = ''
